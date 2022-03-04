5ac4816551d43d08dec185919991f3d84d5fe1c1
Hilbert-Johnson <1730625285@qq.com>
2022-03-04 22:55:02 +0800

    before lab1.3

--- lab1-201300024/lab/bootloader/start.s
+++ lab1-201300024/lab/bootloader/start.s
@@ -32,6 +32,7 @@
 
 
 /* Protected Mode Hello World */
+/*
 .code16
 .global start
 start:
@@ -96,41 +97,51 @@ gdt:
 gdtDesc:
     .word (gdtDesc - gdt -1)
     .long gdt
-
+*/
 
 
 /* Protected Mode Loading Hello World APP */
-#  .code16
-#  .global start
-#  start:
-#      cli                             #关闭中断
-#                                      #启动A20总线
-    
-    
-#      data32 addr32 lgdt gdtDesc      #加载GDTR
-#                                      #启动保护模式
-    
-#                                      #设置CR0的PE位（第0位）为1
-#      data32 ljmp $0x08, $start32     #长跳转切换至保护模式
+.code16
+.global start
+start:
+    cli                             #关闭中断                      
+	inb $0x92,%al                   #启动A20总线
+    orb $0x02,%al
+	outb %al,$0x92
+	                                
+    data32 addr32 lgdt gdtDesc      #加载GDTR
+    movl %cr0, %eax                 #启动保护模式
+    orb $0x01, %al                  #设置CR0的PE位（第0位）为1
+	movl %eax, %cr0                 
+    data32 ljmp $0x08, $start32     #长跳转切换至保护模式
 
-#  .code32
-#  start32:
-#      ...                             #初始化DS ES FS GS SS 初始化栈顶指针ESP
-# 	jmp bootMain # jump to bootMain in boot.c
+.code32
+start32:
+    movw $0x10, %ax                 #初始化DS ES FS GS SS 初始化栈顶指针ESP
+	movw %ax, %ds
+	movw %ax, %es
+	movw %ax, %fs
+	movw %ax, %ss
+	movw $0x18, %ax 
+	movw %ax, %gs
+	movl $0x7ce0, %eax 
+	movl %eax, %esp
 
-# gdt:
-#     .word                           #GDT第一个表项必须为空
-#     .byte 
-#     .word                           #代码段描述符
-#     .byte 
+ 	jmp bootMain                    #jump to bootMain in boot.c
+
+gdt:
+    .word 0,0                       #GDT第一个表项必须为空
+    .byte 0,0,0,0
+
+    .word 0xffff,0x0000             #代码段描述符
+    .byte 0x00,0x9a,0xcf,0x00
         
-#     .word                           #数据段描述符
-#     .byte 
+    .word 0xffff,0x0000             #数据段描述符
+    .byte 0x00,0x92,0xcf,0x00
         
-#     .word                           #视频段描述符
-#     .byte 
-#     ...
+    .word 0xffff,0x7ce0             #视频段描述符
+    .byte 0x0b,0x92,0xcf,0x00
 
-# gdtDesc:
-#     .word (gdtDesc - gdt -1)
-#     .long gdt
+gdtDesc:
+    .word (gdtDesc - gdt -1)
+    .long gdt
